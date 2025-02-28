4b00dd9ef55a4ab4380c64bcbd00ebca23b10b9d
Reynaldo Faria <71574458+reyfaria@users.noreply.github.com>
2025-02-27 23:31:50 -0300

    Update frame.js

--- api/frame.js
+++ api/frame.js
@@ -1,11 +1,11 @@
 export default function handler(req, res) {
     res.status(200).json({
       version: "vNext",
-      image: "https://wallpaperaccess.com/full/1940042.jpg", // Cambia esto a la URL correcta de la imagen del frame
+      image: "https://wallpaperaccess.com/full/1940042.jpg", // Imagen del frame
       buttons: [
         { label: "Abrir Carrusel", action: "post", target: "https://framev2test.vercel.app/api/frame-action" }
       ],
-      aspectRatio: "1.91:1"
+      aspectRatio: "1.91:1",
+      splashImageUrl: "https://wallpaperaccess.com/full/1940042.jpg" // Agrega esta línea con una imagen válida
     });
-  }
-  
+}
