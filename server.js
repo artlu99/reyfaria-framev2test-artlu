61b2b4e23640e20f4979a6f94073d54ce1e374ef
Reynaldo Faria <71574458+reyfaria@users.noreply.github.com>
2025-02-27 23:35:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware para parsear el cuerpo de las solicitudes en formato JSON
 app.use(express.json());
 
-// Endpoint para manejar las acciones del Frame
 app.post('/frame-action', (req, res) => {
   const { buttonIndex } = req.body;
 
-  // Lógica para el botón "Siguiente"
   if (buttonIndex === 1) {
     res.json({
       type: 'frame',
@@ -19,9 +16,7 @@ app.post('/frame-action', (req, res) => {
         {"label": "Anterior", "action": "post"}
       ]
     });
-  }
-  // Lógica para el botón "Anterior"
-  else if (buttonIndex === 2) {
+  } else if (buttonIndex === 2) {
     res.json({
       type: 'frame',
       image: 'https://wallpaperaccess.com/full/1940042.jpg',
@@ -30,14 +25,11 @@ app.post('/frame-action', (req, res) => {
         {"label": "Anterior", "action": "post"}
       ]
     });
-  }
-  // Si el botón no es válido
-  else {
+  } else {
     res.status(400).send('Acción no válida');
   }
 });
 
-// Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
