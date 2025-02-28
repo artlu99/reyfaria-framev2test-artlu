611717353194589f88afd571ac20b6f9981a0021
Reynaldo Faria <71574458+reyfaria@users.noreply.github.com>
2025-02-27 23:30:24 -0300

    Update frame.js

--- api/frame.js
+++ api/frame.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     res.status(200).json({
       version: "vNext",
-      image: "https://framev2test.vercel.app/frame-image.jpg", // Cambia esto a la URL correcta de la imagen del frame
+      image: "https://wallpaperaccess.com/full/1940042.jpg", // Cambia esto a la URL correcta de la imagen del frame
       buttons: [
         { label: "Abrir Carrusel", action: "post", target: "https://framev2test.vercel.app/api/frame-action" }
       ],
