ae03ec6b206471e8bd7bb197d7d66c1b4e3f645b
Reynaldo Faria <71574458+reyfaria@users.noreply.github.com>
2025-02-27 21:18:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Usa el puerto proporcionado por Vercel
 
 // Middleware para parsear el cuerpo de las solicitudes en formato JSON
 app.use(express.json());
